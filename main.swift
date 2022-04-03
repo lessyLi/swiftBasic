15d2773ce3c0cf4c15353f5184b11ac7e5341e83
lessyLi <101655614+lessyLi@users.noreply.github.com>
2022-04-03 20:48:47 +0300

    Update main.swift

--- main.swift
+++ main.swift
@@ -106,4 +106,207 @@ for i in 2...1000 {
     }
 }
 
+// Третья домашка
+
+///  Перечисления
+enum Engine {
+    case started
+    case off
+}
+    
+enum Windows {
+    case opened
+    case closed
+}
+    
+
+/// Структура: Легковой автомобиль
+struct SportCar {
+    
+    let brand: String
+    let year: Int
+    let trunk: Double
+    var isEngineStarted = Engine.off {
+        didSet {
+            if isEngineStarted == Engine.started {
+                print("Поехали!\n")
+            } else {
+                print("Приехали\n")
+            }
+        }
+    }
+    var isWindowsOpened = Windows.closed {
+        didSet {
+            if isWindowsOpened == Windows.closed {
+                print("Все окна закрыли, больше не дует!\n")
+            } else {
+                print("Все окна открыли, не высовывайся!\n")
+            }
+        }
+    }
+    var trunkOccupied = 0.0 {
+        didSet {
+            if trunkOccupied >= trunk {
+                print("Багажник не резиновый!\n")
+            } else {
+                print("Еще осталось немного места")
+            }
+        }
+    }
+    var trunkFree: Double {
+        get {
+            return trunk - trunkOccupied
+        }
+        set {
+            trunkOccupied += newValue
+        }
+    }
+    
+    mutating func offEngine(_ status: Engine) {
+        self.isEngineStarted = .off
+    }
+    
+    mutating func startedEngine(_ status: Engine) {
+        self.isEngineStarted = .started
+    }
+    
+    mutating func openWindows(_ status: Windows) {
+        self.isWindowsOpened = .opened
+    }
+    
+    mutating func closeWindows(_ status: Windows) {
+        self.isWindowsOpened = .closed
+    }
+    
+    mutating func trunkLoading(litres: Double) {
+        self.trunkOccupied += litres
+    }
+    
+    mutating func trunkUnloading(litres: Double) {
+        self.trunkOccupied -= litres
+    }
+    
+}
+
+/// Создаю экземпляр легкового авто
+print("\n\n-------------------Лековой авто-------------------------------\n\n")
+
+var toyota = SportCar(brand: "Toyota", year: 2017, trunk: 50.0)
+print("Появление экземпляра:\n\(toyota)\n")
+
+print("Проверяем сколько свободного места в багажнике:\n\(toyota.trunkFree)\n")
+
+toyota.trunkOccupied = 10.0
+print("Изменили начальную заполненность багажника: \n\(toyota)\n")
+
+toyota.trunkLoading(litres: 30.0)
+print("Загрузили 30.0 в багажник:\n\(toyota)\n")
+
+print("Проверяем, сколько сободного места осталось в багажнике:\n\(toyota.trunkFree)\n")
+
+toyota.isEngineStarted = Engine.started
+print("Запустили двигатель:\n\(toyota)\n")
+
+toyota.isWindowsOpened = Windows.opened
+print("У экземпляра открылись окна:\n\(toyota)\n")
+
+toyota.isEngineStarted = Engine.off
+print("Заглушили двигатель:\n\(toyota)\n")
+
+toyota.isWindowsOpened = Windows.closed
+print("У экземпляра все окна закрыты:\n\(toyota)\n")
+
+
+/// Структура: Грузовой автомобиль
+struct TrunkCar {
+    
+    let brand: String
+    let year: Int
+    let trunk: Double
+    var isEngineStarted = Engine.off {
+        didSet {
+            if isEngineStarted == Engine.started {
+                print("Поехали!\n")
+            } else {
+                print("Приехали\n")
+            }
+        }
+    }
+    var isWindowsOpened = Windows.closed {
+        didSet {
+            if isWindowsOpened == Windows.closed {
+                print("Все окна закрыли, больше не дует!\n")
+            } else {
+                print("Все окна открыли, не высовывайся!\n")
+            }
+        }
+    }
+    var trunkOccupied = 0.0 {
+        didSet {
+            if trunkOccupied >= trunk {
+                print("Багажник не резиновый!\n")
+            } else {
+                print("Еще осталось немного места")
+            }
+        }
+    }
+    var trunkFree: Double {
+        get {
+            return trunk - trunkOccupied
+        }
+        set {
+            trunkOccupied += newValue
+        }
+    }
+    
+    mutating func offEngine(_ status: Engine) {
+        self.isEngineStarted = .off
+    }
+    
+    mutating func startedEngine(_ status: Engine) {
+        self.isEngineStarted = .started
+    }
+    
+    mutating func openWindows(_ status: Windows) {
+        self.isWindowsOpened = .opened
+    }
+    
+    mutating func closeWindows(_ status: Windows) {
+        self.isWindowsOpened = .closed
+    }
+    
+    mutating func trunkLoading(litres: Double) {
+        self.trunkOccupied += litres
+    }
+    
+    mutating func trunkUnloading(litres: Double) {
+        self.trunkOccupied -= litres
+    }
+    
+}
+
+/// Создаю экземпляр грузового авто
+print("\n\n----------------------Грузовой авто----------------------------\n\n")
+
+var man = TrunkCar(brand: "MAN", year: 2015, trunk: 1_000.0)
+print("Появление экземпляра MAN:\n\(man)\n")
+
+print("Проверяем сколько свободного места в багажнике:\n\(man.trunkFree)\n")
+
+man.trunkOccupied = 100.0
+print("Изменили начальную заполненность багажника: \n\(man)\n")
+
+man.trunkLoading(litres: 500.0)
+print("Загрузили 500.0 в багажник:\n\(man)\n")
+
+print("Проверяем, сколько сободного места осталось в багажнике:\n\(man.trunkFree)\n")
+
+man.isEngineStarted = Engine.started
+print("Запустили двигатель:\n\(man)\n")
+
+man.isEngineStarted = Engine.off
+print("Заглушили двигатель:\n\(man)\n")
+
+man.trunkUnloading(litres: 300.0)
+print("Выгрузили 300.0 из багажника:\n\(man)\n")
 
